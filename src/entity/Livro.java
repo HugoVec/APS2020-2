84925ed0f47f8f545e070d0bdb8ae50c1f90b395
HugoVec <42803993+HugoVec@users.noreply.github.com>
2020-09-21 18:51:56 -0300

    Entidade Livro

--- src/entity/Livro.java
+++ src/entity/Livro.java
@@ -5,7 +5,31 @@ public class Livro {
 	private String titulo;
 	private String isbn;
 	private int publisher_id;
+	private float price;
 	
-	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPublisher_id() {
+		return publisher_id;
+	}
+	public void setPublisher_id(int publisher_id) {
+		this.publisher_id = publisher_id;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
 
 }
