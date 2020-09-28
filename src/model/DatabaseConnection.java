8338f621f55ef822a10f8739d82cbde54a2cd37d
HugoVec <42803993+HugoVec@users.noreply.github.com>
2020-09-28 18:36:58 -0300

    inciando a conexão com o banco

--- src/model/DatabaseConnection.java
+++ src/model/DatabaseConnection.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+	static final String JDBCURL = "jdbc:postgresql://localhost/APS";
+    static final String USERNAME = "postgres";
+    static final String PASSWORD = "123";
+    
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(JDBCURL, USERNAME, PASSWORD);  
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        DatabaseConnection sqlConnect = new DatabaseConnection();
+        sqlConnect.connect();
+    }
+    
+    
 
 }
